c31ee896f7512cfb1e160f2451dbcd56d385fb4c
BirdZhang <0312birdzhang@gmail.com>
2022-01-10 23:00:39 +0800

    some updates

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo -1000 > /proc/self/oom_score_adj
-echo "pass"
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+parse-android-dynparts /dev/sda23
+mount /dev/mapper/dynpart-system_a  /system
+mount /dev/mapper/dynpart-system_ext_a /system_ext
+mount /dev/mapper/dynpart-vendor_a  /vendor
+mount /dev/mapper/dynpart-odm_a   /odm
+mount /dev/mapper/dynpart-product_a /product
+
