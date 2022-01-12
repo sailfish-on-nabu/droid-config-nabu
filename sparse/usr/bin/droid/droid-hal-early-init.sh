f41804f9d78a2827ba95ae5f4da2c14bb904f674
BirdZhang <0312birdzhang@gmail.com>
2022-01-12 21:39:07 +0800

    fixup partitions mount

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-parse-android-dynparts /dev/sda23
-mount /dev/mapper/dynpart-system_a  /system
-mount /dev/mapper/dynpart-system_ext_a /system_ext
-mount /dev/mapper/dynpart-vendor_a  /vendor
-mount /dev/mapper/dynpart-odm_a   /odm
-mount /dev/mapper/dynpart-product_a /product
+losetup -r /dev/loop0 /dev/disk/by-partlabel/super
+dmsetup create --concise "$(parse-android-dynparts /dev/loop0)"
 
+mkdir -p /product /system_ext /odm /system_root /system
+
+mount /dev/mapper/dynpart-system_a /system_root
+mount /dev/mapper/dynpart-vendor_a /vendor
+mount /dev/mapper/dynpart-product_a /product
+mount /dev/mapper/dynpart-system_ext_a /system_ext
+mount /dev/mapper/dynpart-odm_a /odm
+
+mount --bind /system_root/system /system
