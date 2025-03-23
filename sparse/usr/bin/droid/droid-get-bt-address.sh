1d272cc4922fa13de0eb9b41a08ae9f7cc73d33a
BirdZhang <0312birdzhang@gmail.com>
2025-03-23 17:34:29 +0800

    bt address

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -10,3 +10,12 @@
 
 # copy from https://github.com/VerdandiTeam/droid-config-pipa/blob/master/sparse/usr/bin/droid/droid-get-bt-address.sh
 find /var/lib/bluetooth -maxdepth 1 -iname '*:*:*:*:*:*' | cut -d/ -f 5 > /var/lib/bluetooth/board-address
+# don't know why it's not working
+# let's recheck
+B=$(cat /var/lib/bluetooth/board-address)
+
+echo "BT MAC: $B"
+if [ ! -z "$B" ] ; then
+    bt_mac=${B:0:2}:${B:2:2}:${B:4:2}:${B:6:2}:${B:8:2}:${B:10:2}
+    echo $bt_mac > /var/lib/bluetooth/board-address
+fi
