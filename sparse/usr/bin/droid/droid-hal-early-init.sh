3249ae01d11cbb55db83a87836835503a7ce56bc
BirdZhang <0312birdzhang@gmail.com>
2026-05-27 21:40:55 +0800

    does not need these for sound

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -5,11 +5,6 @@ mount --bind /odm_root/etc /odm/etc
 
 # wlan
 mount --bind /lib/modules/`uname -r`/wlan.ko /vendor/lib/modules/qca_cld3_wlan.ko
-mount --bind /usr/libexec/droid-hybris/system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.msmnile.so
 
 # bt
 mount --bind /usr/libexec/droid-hybris/system/bluebinder_wait.sh /usr/bin/droid/bluebinder_wait.sh
-
-
-# audio
-mount --bind /usr/libexec/droid-hybris/vendor/etc/audio/audio_policy_configuration.xml /vendor/etc/audio/audio_policy_configuration.xml
